4001044e20cdb1a210de6e8c9bb13fd6d701ad6e
MarceloFormentini <marceloformentini74@gmail.com>
2025-03-09 15:38:36 -0300

    correção no nome da tabela

--- src/model/entities/users.py
+++ src/model/entities/users.py
@@ -3,10 +3,9 @@ from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.sql import func
 
 class Users(Base):
-	__tablename__ = 'task'
+	__tablename__ = 'users'
 	id = Column(Integer, primary_key=True, autoincrement=True)
 	name = Column(String(100), nullable=False)
 	email = Column(String(100), nullable=False)
 	password = Column(String(100), nullable=False)
-
 	created_at = Column(DateTime, nullable=False, default=func.now())
